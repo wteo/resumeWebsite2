916b064e7d0c5b4c533c68a40f9f12eb2736cc21
wteo <wendyteo.wy@gmail.com>
2024-08-10 08:25:05 +0200

    Updated style

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -36,24 +36,24 @@ function App() {
       <header className="header__menu">
       </header>
       <section className="folder" style={{ transform: isVisible ? 'translateY(0)' : 'translateY(100%)', opacity: isVisible ? 1 : 0 }}>
-        <div className="file blue">
+        <div className="file yellow">
           <h3>CONTACT ME</h3>
           <hr />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, ligula eget bibendum fermentum, nulla metus facilisis enim, in efficitur eros purus in nisl. Vivamus interdum, felis id consequat ullamcorper, dui elit tincidunt velit, at efficitur sem orci sit amet lorem. Fusce euismod, mauris id convallis facilisis, erat orci congue neque, in sagittis mauris nulla non felis. Curabitur id dictum est, non varius turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam erat volutpat. Duis nec nisi at orci feugiat gravida.</p>
           <p>Suspendisse potenti. Nam a ligula nec risus vehicula venenatis. Fusce dignissim tristique augue, a hendrerit lorem interdum et. Curabitur at dapibus nisi. Mauris sodales, velit nec egestas vehicula, augue tortor fermentum erat, ac consequat magna nunc ut justo. Aenean tincidunt semper ipsum non facilisis. Aenean quis suscipit lorem.</p>
         </div>
-        <div className="file green">
+        <div className="file forest">
           <h3>ABOUT ME</h3>
           <hr />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, ligula eget bibendum fermentum, nulla metus facilisis enim, in efficitur eros purus in nisl. Vivamus interdum, felis id consequat ullamcorper, dui elit tincidunt velit, at efficitur sem orci sit amet lorem. Fusce euismod, mauris id convallis facilisis, erat orci congue neque, in sagittis mauris nulla non felis. Curabitur id dictum est, non varius turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam erat volutpat. Duis nec nisi at orci feugiat gravida.</p>
         </div>
-        <div className="file pink">
+        <div className="file coral">
           <h3>MY SERVICE</h3>
           <hr />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, ligula eget bibendum fermentum, nulla metus facilisis enim, in efficitur eros purus in nisl. Vivamus interdum, felis id consequat ullamcorper, dui elit tincidunt velit, at efficitur sem orci sit amet lorem. Fusce euismod, mauris id convallis facilisis, erat orci congue neque, in sagittis mauris nulla non felis. Curabitur id dictum est, non varius turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam erat volutpat. Duis nec nisi at orci feugiat gravida.</p>
           <p>Suspendisse potenti. Nam a ligula nec risus vehicula venenatis. Fusce dignissim tristique augue, a hendrerit lorem interdum et. Curabitur at dapibus nisi. Mauris sodales, velit nec egestas vehicula, augue tortor fermentum erat, ac consequat magna nunc ut justo. Aenean tincidunt semper ipsum non facilisis. Aenean quis suscipit lorem.</p>
         </div>
-        <div className="file white">
+        <div className="file lime">
           <h3>MY SELECTED WORKS</h3>
           <hr />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, ligula eget bibendum fermentum, nulla metus facilisis enim, in efficitur eros purus in nisl. Vivamus interdum, felis id consequat ullamcorper, dui elit tincidunt velit, at efficitur sem orci sit amet lorem. Fusce euismod, mauris id convallis facilisis, erat orci congue neque, in sagittis mauris nulla non felis. Curabitur id dictum est, non varius turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam erat volutpat. Duis nec nisi at orci feugiat gravida.</p>
