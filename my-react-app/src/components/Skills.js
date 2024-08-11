299fe88feeea00a369df88df19afbaece99f19fe
wteo <wendyteo.wy@gmail.com>
2024-08-11 06:21:06 +0200

    Updated style

--- my-react-app/src/components/Skills.js
+++ my-react-app/src/components/Skills.js
@@ -35,13 +35,13 @@ function Tools() {
 
     return (
         <section className="skills">
-            <h1>Skills</h1>
+            <h1 className="skills__title">Skills</h1>
             <hr />
             <div className="skills__group">
                 {
                     categories.map((category, categoryIndex) => (
                         <div className="skills__item" key={categoryIndex}>
-                            <h4 className="skills__title">{category.category}</h4>
+                            <h4 className="skills__category">{category.category}</h4>
                             <p className="skills__description">{ category.description }</p>
                             <ul className="skills__list">Tools Used: 
                                 {category.tools.map((tool, toolIndex) => <li key={toolIndex}>{tool}</li>)}
