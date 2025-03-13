fb9e8c695e9350d6f2431f23d839febeb4dd39b7
wteo <wendyteo.wy@gmail.com>
2025-03-13 12:42:39 +1100

    Updated year

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -31,7 +31,7 @@ function Footer() {
                         <p className="footer__contact-detail">+61 458 369 510</p>
                     </div>
                 </div>
-                <p className="footer__copyright">© Wendy Teo 2024</p>
+                <p className="footer__copyright">© Wendy Teo 2025</p>
             </ContentWrapper>
         </footer>
     );
